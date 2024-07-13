70ad686d05e78b3e71ff36e65ca24fbe93904d4e
Joseph Lumbley <joelumbley16@gmail.com>
2024-07-13 01:10:16 -0500

    Added Portal2 sound to resources

--- Platformer with Level Editor/My Project/Resources.Designer.vb
+++ Platformer with Level Editor/My Project/Resources.Designer.vb
@@ -136,5 +136,15 @@ Namespace My.Resources
                 Return CType(obj,Byte())
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
+        Friend ReadOnly Property Portal2() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Portal2", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
     End Module
 End Namespace

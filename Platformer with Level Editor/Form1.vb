3b74f1f84c9280ae765692f0eb34efad18b30151
Joseph Lumbley <joelumbley16@gmail.com>
2024-07-15 13:44:19 -0500

    Clean up

--- Platformer with Level Editor/Form1.vb
+++ Platformer with Level Editor/Form1.vb
@@ -1293,7 +1293,7 @@ Public Class Form1
 
         If Connected(0) = False AndAlso Connected(1) = True Then
 
-            VibrateLeft(0, 65000)
+            VibrateLeft(1, 65000)
 
             VibrateRight(1, 65000)
 
